65238fe3f996a76a4d3c43b6ff53850333d01c38
sandeep singh sandha <sandha.iitr@gmail.com>
2018-11-20 21:35:51 +0000

    jetson setup

--- computation/Jetson/utils/visualization_utils.py
+++ computation/Jetson/utils/visualization_utils.py
@@ -24,8 +24,8 @@ from abc import abstractmethod
 import collections
 import functools
 # Set headless-friendly backend.
-import matplotlib; matplotlib.use('Agg')  # pylint: disable=multiple-statements
-import matplotlib.pyplot as plt  # pylint: disable=g-import-not-at-top
+#import matplotlib; matplotlib.use('Agg')  # pylint: disable=multiple-statements
+#import matplotlib.pyplot as plt  # pylint: disable=g-import-not-at-top
 import numpy as np
 import PIL.Image as Image
 import PIL.ImageColor as ImageColor

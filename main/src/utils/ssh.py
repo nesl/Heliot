313a9e93c864442b6e2936ae5aed11e035637dab
sandeep singh sandha <sandha.iitr@gmail.com>
2019-01-01 17:42:45 -0800

    core files for devices

--- main/src/utils/ssh.py
+++ main/src/utils/ssh.py
@@ -7,6 +7,67 @@ During initialization ssh connection is used to download heliot repo from github
 
 import paramiko
 import sys
+import os as sys_os
+
+class MySFTPClient(paramiko.SFTPClient):
+    def put_dir(self, source, target):
+        ''' Uploads the contents of the source directory to the target path. The
+            target directory needs to exists. All subdirectories in source are
+            created under target.
+        '''
+        for item in sys_os.listdir(source):
+            if sys_os.path.isfile(sys_os.path.join(source, item)):
+                self.put(sys_os.path.join(source, item), '%s/%s' % (target, item))
+            else:
+                self.mkdir('%s/%s' % (target, item), ignore_existing=True)
+                self.put_dir(sys_os.path.join(source, item), '%s/%s' % (target, item))
+
+    def mkdir(self, path, mode=511, ignore_existing=False):
+        ''' Augments mkdir by adding an option to not fail if the folder exists  '''
+        try:
+            super(MySFTPClient, self).mkdir(path, mode)
+        except IOError:
+            if ignore_existing:
+                pass
+            else:
+                raise
+
+
+
+# Transfer heliot repo from master to the device
+def transfer_heliot_repo_from_master(ip, username, password, logger):
+
+    try:
+        port = 22
+        transport = paramiko.Transport((ip,port) )
+        #transport.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        transport.connect(username=username, password=password)
+
+        sftp = MySFTPClient.from_transport(transport)
+
+
+
+        # Upload
+
+        target_path = '/home/nvidia/heliot_runtime/'
+        source_path = '/home/nesl/Heliot/github/Heliot/'
+
+        sftp.mkdir(target_path, ignore_existing=True)
+        sftp.put_dir(source_path, target_path)
+
+
+        sftp.close()
+
+        transport.close()
+
+
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+
 
 def do_intializaton_linux(ip, username, password, logger):
     try:
@@ -35,6 +96,10 @@ def do_intializaton_linux(ip, username, password, logger):
         output = stdout.readlines()
         #print('output is:',str(output[0]),":" ,type(output))
 
+        if len(output) == 0:
+            logger.error('heliot_runtime directory cannot be created')
+            sys.exit()
+
         if str(output[0])!= 'exist\n':
             logger.error('heliot_runtime directory cannot be created')
             sys.exit()
@@ -52,24 +117,28 @@ def do_intializaton_linux(ip, username, password, logger):
         exit_status = stdout.channel.recv_exit_status()          # Blocking call
 
         output = stdout.readlines()
-        #print('output is:',str(output[0]),":" ,type(output))
+        # print('output is:',output,":" ,type(output))
+        # outputerr = stderr.readlines()
+        # print('outputerr is:',outputerr,":" ,type(outputerr))
+
+        if len(output)==0:
+            logger.info('Not able to download Heliot repo from Github')
+            logger.info('Trying to transfer Heliot repo from master to device')
+            if not transfer_heliot_repo_from_master(ip, username, password, logger):
+                logger.error('Not able to download Heliot repo from Github')
+                logger.error('Not able to transfer Heliot repo from master to device')
+                sys.exit()
 
         if str(output[0])!= 'exist\n':
             logger.error('Not able to download Heliot repo from Github')
             sys.exit()
 
         logger.info('Heliot repo downloaded from Github')
-        client.close()
-
-
-        # A new client to start a non-blocking call
-        client = paramiko.SSHClient()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(ip, timeout=5, look_for_keys=False, username=username, password=password)
 
         cmd = 'cd heliot_runtime/Heliot/main/src; python3 init.py &'
         stdin, stdout, stderr = client.exec_command(cmd)
 
+        #Note should not try to read the output, as this starts a continuous process, and this call will not return
         # output = stdout.readlines()
         # print('stdout output is:',output)
         #

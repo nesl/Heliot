801bd2be82079ce63355ab658672f7feaa6e3a25
sandeep singh sandha <sandha.iitr@gmail.com>
2019-02-02 16:46:21 -0800

    mininet dynamic topology

--- main/src/network/netHeliot.py
+++ main/src/network/netHeliot.py
@@ -74,27 +74,27 @@ class netHeliot:
 
 
 #Local testing
-net1 = netHeliot()
-
-net1.add_switch('s1')
-net1.add_host('h1')
-net1.add_host('h2')
-
-s1 = net1.get_switch('s1')
-h1 = net1.get_host('h1')
-h2 = net1.get_host('h2')
-
-net1.add_link(s1,h1)
-net1.add_link(s1,h2)
-
-
-net1._network.start()
-
-print "h1.IP():",h1.IP()
-print "h2.IP():",h2.IP()
-print "s1.IP():",s1.IP()
-
-
-print h1.cmd('ping -c1 %s' % h2.IP())
-
-net1._network.stop()
+# net1 = netHeliot()
+#
+# net1.add_switch('s1')
+# net1.add_host('h1')
+# net1.add_host('h2')
+#
+# s1 = net1.get_switch('s1')
+# h1 = net1.get_host('h1')
+# h2 = net1.get_host('h2')
+#
+# net1.add_link(s1,h1)
+# net1.add_link(s1,h2)
+#
+#
+# net1._network.start()
+#
+# print "h1.IP():",h1.IP()
+# print "h2.IP():",h2.IP()
+# print "s1.IP():",s1.IP()
+#
+#
+# print h1.cmd('ping -c1 %s' % h2.IP())
+#
+# net1._network.stop()

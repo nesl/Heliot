7d986790993b5b33b09971258ca655603e76e719
sandeep singh sandha <sandha.iitr@gmail.com>
2019-03-01 17:49:38 -0800

    updated android

--- demo/task_android.py
+++ demo/task_android.py
@@ -1,35 +1,35 @@
 from utils.dataflow import *
 
 from flask import Flask
+import _thread
 
 
 app = Flask(__name__)
 
-DISPLAY_IP = None
-DISPLAY_PORT = None
+labels = []
+
+def get_data():
+    global labels
+    while True:
+        label = dataflow.getData(inport=20003) #get the labels
+        if label!=None:
+            if len(label)>0:
+                labels.append(label[0])
+        else:
+            labels.append('None')
+        print('labels:',labels)
+        if len(labels)>5:
+            labels = []
 
 @app.route("/")
 def hello():
-    labels = dataflow.getData(inport=20003) #get the labels
+    global labels
     print('lables are:',labels)
-    result = None
-    if labels!=None:
-        if len(labels)>0:
-            result = labels[0]
-            #if result !='car' or result !='person':
-                #result =None
-    return '{}'.format(result)
+    return '{}'.format(labels)
 
 
 if __name__ == '__main__':
-    # if len(sys.argv) != 3:
-    #     print(
-    #         'usage: python {0} WEB_IP:WEB_PORT DISPLAY_IP:DISPLAY_PORT. \n'
-    #         'e.g. python {0} 172.17.51.1:7788 172.17.51.1:18900'.format(
-    #             sys.argv[0]))
-    #     exit(0)
-    # print('{} running at {} and getting data from {}'.format(
-    #     sys.argv[0], sys.argv[1], sys.argv[2]))
-    web_ip, web_port = '172.17.15.21','7788'#sys.argv[1].split(':')
-    #DISPLAY_IP, DISPLAY_PORT = sys.argv[2].split(':')
+    _thread.start_new_thread( get_data, () )
+
+    web_ip, web_port = '192.168.1.6','7788'
     app.run(host=web_ip, port=int(web_port))

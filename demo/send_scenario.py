6d843cbf8144dfcb25d0e4e0b086f52fb43f1ddb
sandeep singh sandha <sandha.iitr@gmail.com>
2019-10-12 16:03:35 -0700

    send Scenario code

--- demo/send_scenario.py
+++ demo/send_scenario.py
@@ -1,6 +1,13 @@
 
 # ip of the airsim machine need to be fixed to send sceanrio
 from utils.dataflow import *
-scenario = 0
-result = dataflow.sendData(id='air_sim_scenario',data=scenario)
-print(result)
+import sys
+
+scenario = 1
+
+
+if __name__ == "__main__":
+    if len(sys.argv)==2:
+        scenario=int(sys.argv[1])
+    result = dataflow.sendData(id='air_sim_scenario',data=scenario)
+    print(result)

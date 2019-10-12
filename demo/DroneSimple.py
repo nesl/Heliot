f003af12b20e414b56ba8c4022c8a40f614bad17
sandeep singh sandha <sandha.iitr@gmail.com>
2019-10-12 13:46:32 -0800

    drone weather effect

--- demo/DroneSimple.py
+++ demo/DroneSimple.py
@@ -11,6 +11,15 @@ client.armDisarm(True)
 
 client.takeoffAsync().join()
 
-client.moveToPositionAsync(0, 0,-2, 10,yaw_mode=airsim.YawMode(is_rate=False, yaw_or_rate=90),drivetrain=airsim.DrivetrainType.MaxDegreeOfFreedom).join()
+client.simEnableWeather(True)
+#client.moveToPositionAsync(0, 0,-2, 10,yaw_mode=airsim.YawMode(is_rate=False, yaw_or_rate=90),drivetrain=airsim.DrivetrainType.MaxDegreeOfFreedom).join()
+client.simSetWeatherParameter(airsim.WeatherParameter.Rain, 0.25);
+client.simSetWeatherParameter(airsim.WeatherParameter.Dust, 0.50);
+
+client.moveToPositionAsync(100, 0,-2, 5,yaw_mode=airsim.YawMode(is_rate=False, yaw_or_rate=90),drivetrain=airsim.DrivetrainType.MaxDegreeOfFreedom).join()
+
+client.simEnableWeather(False)
+
+client.moveToPositionAsync(0, 0,-2, 5,yaw_mode=airsim.YawMode(is_rate=False, yaw_or_rate=90),drivetrain=airsim.DrivetrainType.MaxDegreeOfFreedom).join()
 
 client.reset()
